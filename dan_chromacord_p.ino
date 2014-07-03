df5c3b9b82156d54bcf38d67dc7cc9109fda3aed
Alan Grover <awgrover@msen.com>
2014-07-03 17:18:07 -0400

    basic slider to zone mapping

--- dan_chromacord_p.ino
+++ dan_chromacord_p.ino
@@ -1,7 +1,8 @@
-#include <hsv2rgb.h>
-#include <pixeltypes.h>
+// #include <hsv2rgb.h>
+// #include <pixeltypes.h>
 #include <Wire.h>
 #include <TLC59116.h>
+#include <MsTimer2.h>
 
 #define start_sequence { \
   static byte sequence_i = 0; \
@@ -20,9 +21,6 @@
   }
 #define end_do_sequence end_sequence }
 
-// CHSV hsv;
-CRGB rgb;
-CRGB rgb2;
 TLC59116 tlc_first;
 TLC59116* tlc[14]; // null'd
 
@@ -44,15 +42,6 @@ void setup() {
       }
     }
   Serial.print("Made tlc[0..");Serial.print(tlc_count-1);Serial.print("] out of "); Serial.println(scanner.count());
-  }
-
-byte* rgb2array(CRGB &rgb) {
-  // 3 bytes {r,g,b}
-  static byte as_array[3];
-  as_array[0] = rgb.red;
-  as_array[1] = rgb.green;
-  as_array[2] = rgb.blue;
-  return as_array;
   }
 
 void loop() {
@@ -104,15 +93,33 @@ void loop() {
       test_num = 0xff;
       break;
 
+    case 'g' : // Go into performance mode
+      performance();
+      test_num = 0xfe;
+      break;
+
+    case 't' : // Timer test: make something blink every n
+      MsTimer2::set(200, on_off_isr);
+      MsTimer2::start();
+      while(Serial.available() <= 0) {
+        Serial.println(analogRead(3));
+        }
+      MsTimer2::stop();
+      test_num = 0xfe;
+      break;
+      
     default:
       Serial.println();
       // menu made by: make 
-      // include dan_callibration.ino.menu
-      // end-menu
+      // include *.ino.menu
 Serial.println(F("0  1st 3 rgb on/off/hsv sanity"));
+Serial.println(F("o  on"));
 Serial.println(F("r  Reset"));
 Serial.println(F("s  Spiral through HSB color Space"));
 Serial.println(F("p  Pwm full range"));
+Serial.println(F("z  huh"));
+Serial.println(F("g  Go into performance mode"));
+      // end-menu
 
       Serial.println(F("? Prompt again"));
       test_num = 0xFF; // prompt
@@ -135,8 +142,7 @@ void prove_on() {
   Serial.println("off...");
   tlc_first.off(0).off(1).off(2)
   .delay(500);
-  rgb = CHSV( 50, 128, 128);
-  tlc_first.pwm(0, 3, rgb2array(rgb))
+  tlc_first.pwm(0, 3, (byte[]){50, 128, 128})
   .delay(500);
   }
 
@@ -157,19 +163,13 @@ void spiral() {
 
         for (byte h=0; ; h++) {
           // Serial.print("H ");Serial.print(h); Serial.print(" "); Serial.println( h<=255);
-          rgb = CHSV(h,s,v);
-          rgb2 = CHSV(second_offset-h,s,v);
           for (TLC59116** a_tlc = tlc; *a_tlc; a_tlc++) {
             // for each rgb
-            byte *as_array = rgb2array(rgb);
+            byte as_array[] = { h,s,v };
+            byte as_array2[] = { h-128,s,v };
             tlc_first.pwm(0,3,as_array);
-            tlc_first.pwm(3,3,rgb2array(rgb2));
+            tlc_first.pwm(3,3,as_array2);
             delay(20);
-            /*
-            for (byte group=0; group < max_group_led; group += 3) {
-              (*a_tlc)->pwm(group, 3, as_array);
-              }
-            */
             delay(on_time);
             }
 
@@ -216,4 +216,108 @@ void pwm_full_range() {
     for(byte i=size1; i<=15; i++) { values[i] -= direction; }
     
     }
+  }
+
+void on_off_isr() {
+  static bool oo = 0;
+  sei();
+  tlc_first.on(1, oo);
+  tlc_first.on(2, oo);
+  oo = !oo;
+  }
+
+const byte Slider_ct = 3;
+const int Slider_Pins[] = { 0,1,2,3,4,5,6,7,  8.9,10,11,12,13,14,15 };
+const byte Zones = 1;
+const byte Sample_Time = 100; // microsec
+// 50msec
+const int Sample_Interval = Sample_Time * Slider_ct > 50000 ? (Sample_Time * Slider_ct)/1000 : 50;
+const byte Lunits_Per_Zone = 2;
+const byte LEDs_Per_Lunit = 3;
+const byte Max_Lunit = 5;
+const byte Zone_Assignment[Slider_ct][Lunits_Per_Zone] = { // in Lunits (rgb #)
+  // 0
+  { 0, 1 },
+  // 1
+  { 2,3 },
+  // 2
+  { 4, -1 } // -1 is filler
+  };
+const byte RGB_Assignment[Slider_ct] = { //0,1,2 as rgb
+  0,1,2
+  };
+
+TLC59116 *dandelions[] = {&tlc_first};
+byte Targets[Slider_ct];
+const byte Total_LEDs = LEDs_Per_Lunit * Max_Lunit;
+byte leds[Total_LEDs];
+
+
+void grab_next_slider() {
+  // timer routine
+  for(byte i=0; i<Slider_ct ; i++) {
+    Targets[i] = analogRead(Slider_Pins[i]) >> 2;
+    }
+  }
+
+void performance() {
+  const int break_check_interval = 500;
+
+  // setup interrupt routine to read sliders
+  MsTimer2::set(Sample_Interval, grab_next_slider);
+  MsTimer2::start();
+
+  // update dandelion, till serial.avail
+  unsigned long break_check = millis() + break_check_interval; // every 1/2
+  while(1) {
+
+    for(byte slider_i =0; slider_i < Slider_ct; slider_i++) {
+      // e.g. slider1 => red of zone[1] of rgbs of dandelions
+      Serial.print("Slider ");Serial.print(slider_i);Serial.print(" ");Serial.println(Targets[slider_i]);
+      accumulatepwm_rgb_groups( slider_i );
+      }
+    set_pwm(leds);
+
+    if (millis() > break_check) {
+      if (Serial.available() > 0) { break; }
+      }
+    }
+
+  // cleanup interrupt
+  MsTimer2::stop();
+  }
+
+void set_pwm(byte* leds) {
+  // any assumption of led->actual? 16 per, continous
+  byte number_of_dandelions = (Total_LEDs + 15) / 16;
+  Serial.print("d_ct ");Serial.println(number_of_dandelions);
+  for(byte dandelion_i = 0; dandelion_i < number_of_dandelions; dandelion_i++) {
+    byte start_led_i = dandelion_i * 16;
+    TLC59116* dandelion = dandelions[dandelion_i];
+    dandelion->pwm(&leds[start_led_i]);
+    Serial.print("D ");Serial.print(dandelion_i);
+    for(byte led_num = start_led_i; led_num < start_led_i + 16; led_num++) {
+      Serial.print(leds[led_num]);Serial.print(" ");
+      }
+    Serial.println();
+    }
+  }
+  
+//algorithms
+inline void accumulatepwm_rgb_groups(byte slider_i) {
+  // maps a slider_i to the proper rgb element of zoned lunits
+  const byte* lunit_list = Zone_Assignment[ slider_i / 3 ]; // "zone x"
+  byte rgb_i = RGB_Assignment[slider_i % 3]; // "red of..."
+  
+  Serial.print("Accum for rgbi ");Serial.print(rgb_i);
+  for (byte i=0; i<Lunits_Per_Zone; i++) {
+    byte lunit = lunit_list[i];
+    if (lunit >= 0) {
+      Serial.print("lunit ");Serial.print(lunit);Serial.print(" for ");Serial.print(i);
+      byte t_led_num = lunit * LEDs_Per_Lunit + rgb_i; // "red of zone x"
+      Serial.print(" led num ");Serial.print(t_led_num); Serial.print("=");Serial.print(Targets[slider_i]);
+      leds[t_led_num] = Targets[slider_i];
+      }
+    }
+  Serial.println();
   }
