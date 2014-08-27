db169e45f266fb3479f37f3e0ca76ca07a1d1723
Alan Grover <awgrover@msen.com>
2014-08-27 15:20:34 -0400

    add a test patch for allzones on one. much debugging for the slider->dandelion routine. add desribe. add choose patch. remove legacy/obsolete.

--- dan_chromacord_p.ino
+++ dan_chromacord_p.ino
@@ -127,16 +127,24 @@ void loop() {
       test_num = 0xff;
       break;
 
+    case 'c' : // Choose another patch
+      current_patch_i = choose_patch(current_patch_i);
+      test_num = 0xff;
+      break;
+
     case '?' :
       Serial.println();
       // menu made by: make (in examples/, then insert here)
 Serial.println(F("0  idle/sanity"));
+Serial.println(F("d  Describe actual registers"));
 Serial.println(F("r  Reset"));
 Serial.println(F("C  get max/min of POT on A0 till 'x' (callibration)"));
 Serial.println(F("p  Track Pot"));
 Serial.println(F("P  Track pots with timer & stuff"));
-Serial.println(F("g  Go into performance mode"));
 Serial.println(F("t  Timer test: make something blink every n"));
+Serial.println(F("s  Show patch"));
+Serial.println(F("g  Go into performance mode"));
+Serial.println(F("c  Choose another patch"));
       // end menu
       // fallthrough
 
@@ -239,29 +247,35 @@ void track_print_pots() {
 
 void update_by_dandelion(const byte *zone_rgb[3] /* [Zone_Count][3] */, const byte **patch) {
   // Do each dandelion so we only have to "buffer" 15 values at a time
+  print(F("first pixel @"));print((unsigned int)*patch);Serial.println();
   for(byte dandelion_i=0; dandelion_i < tlcmanager.device_count(); dandelion_i++) {
     // Collect rgb values for this dandelion
-
     TLC59116& dandelion = tlcmanager[dandelion_i];
-    byte pwm_by_rgb[Pixels_Per_Dandelion][3] = {}; // rgb sets
-    print(F("Dandelion ")); print(dandelion.address());Serial.println();
+    byte pwm_buffer[16] = {}; // rgb sets. if no zone-pixel anywhere, then off
+    print(F("pwm buffer "));print((unsigned int) pwm_buffer);Serial.println();
+    print(F("Dandelion 0x")); print(dandelion.address(),HEX);print(F(" i:"));print(dandelion_i);Serial.println();
 
+    // patch: [ zone:[ pixel:i, ...], ... ]
     // FIXME: we should construct a reverse table: zone_i -> [dandelion/channels,...]
-    for (const byte* zone=*patch; zone < *patch + Zone_Count; zone++) {
-      for (const byte zone_i=0; zone_i < Zone_Count; zone++) {
-        print(F("  check zone "));print(zone_i);Serial.println();
-        for (const byte *pix=patch[zone_i]; *pix != -1; pix++) {
-          byte dandelion_for_pixel = *pix / Pixels_Per_Dandelion;
-          if (dandelion_for_pixel == dandelion_i) {
-            byte rgb_i = *pix % Pixels_Per_Dandelion;
-            byte channel = rgb_i * 3; // pixel #2 is at 2*3=channel 6,7,8
-            print(*pix); print(F("->")); print(channel);Serial.println();
-            memcpy(&pwm_by_rgb[channel], (byte* /*[3]*/) zone_rgb[zone_i], 3);
-            }
+    print(F("patch @"));print((unsigned int)patch);print(F(" ptr size"));print(sizeof(patch));Serial.println();
+    for (byte zone_i=0; zone_i < Zone_Count; zone_i++) {
+      print(F("  check zone @"));print((unsigned int)patch + zone_i * sizeof(byte**));print(F(" #"));print(zone_i);Serial.println();
+      print(F("  check zone 1st pixel@"));print((unsigned int)patch[zone_i]);print(F(" #"));print(zone_i);Serial.println();
+      for (const byte* pix = patch[zone_i];  *pix != 0xff; pix++) {
+        byte dandelion_for_pixel = *pix / Pixels_Per_Dandelion;
+        print(F("    pix @"));print((unsigned int)pix);print(F(" #"));print(*pix);print(F(" -> dandelion i "));print(dandelion_for_pixel);Serial.println();
+        if (dandelion_for_pixel == dandelion_i) {
+          byte rgb_i = *pix % Pixels_Per_Dandelion;
+          byte channel = rgb_i * 3; // pixel #2 is at 2*3=channel 6,7,8
+          print(F("    "));print(*pix); print(F("->led #")); print(channel);Serial.println();
+          print(F("    copy rgb[zone_i] to pwm "));print((unsigned int) pwm_buffer + channel);Serial.println();
+          memcpy(pwm_buffer + channel, (byte* /*[3]*/) zone_rgb[zone_i], 3);
+          delay(200);
           }
         }
       }
-    dandelion.pwm(*pwm_by_rgb);
+    print(F("set dandelion"));for(byte i=0; i<16; i++) {print(" ");print(pwm_buffer[i]);}Serial.println();
+    dandelion.pwm(pwm_buffer);
     }
   }
 
@@ -271,7 +285,9 @@ void show_patch(const byte** patch) {
   // tlcmanager[tlcmanager.device_count()-1].on(14);
   byte rgb[Zone_Count][3];
   byte *rgb_p[Zone_Count];
+  // copy pointers so we have byte**
   for(byte i = 0; i<Zone_Count; i++) { rgb_p[i] = rgb[i]; }
+
   for(byte i =0; i < Zone_Count; i++) {
     Serial.print(F("Zone "));Serial.println(i);
     byte rgb_bits = i % 6 + 1; // 1..6, 3bits where 1 or 2 bits are on at a time
@@ -279,9 +295,9 @@ void show_patch(const byte** patch) {
     // set this i to interesting value, set others to 0
     for (byte set_i=0; set_i < Zone_Count; set_i++) {
       if (set_i==i) {
-        rgb[set_i][0] = (rgb_bits & 0b100) ? 100 : 0;
+        rgb[set_i][0] = (rgb_bits & 0b001) ? 100 : 0;
         rgb[set_i][1] = (rgb_bits & 0b010) ? 100 : 0;
-        rgb[set_i][2] = (rgb_bits & 0b001) ? 100 : 0;
+        rgb[set_i][2] = (rgb_bits & 0b100) ? 100 : 0;
         }
       else {
         memset(rgb[set_i], 0, 3);
@@ -299,146 +315,18 @@ void performance(const byte** patch) {
   update_by_dandelion((const byte**)sliders, patch);
   }
 
-/*
-const byte Slider_ct = 3;
-void spiral() {
-  const byte max_group_led = (2 * 3);
-  const unsigned long on_time = 2;
-  const byte v_min = 254;
-  const byte s_by = 10;
-  const byte s_max = (255 / s_by) * s_by;
-  const byte second_offset = 128;
-  while (1) {
-    
-    for(byte v=v_min; ; v++) {
-      Serial.print("V ");Serial.println(v);
-
-      for (byte s=254; ; ) { // s += s_by) {
-        if (!(s % 10)) {Serial.print("S ");Serial.println(s);}
-
-        for (byte h=0; ; h++) {
-          // Serial.print("H ");Serial.print(h); Serial.print(" "); Serial.println( h<=255);
-          for (TLC59116** a_tlc = tlc; *a_tlc; a_tlc++) {
-            // for each rgb
-            byte as_array[] = { h,s,v };
-            byte as_array2[] = { h-128,s,v };
-            tlc_first.pwm(0,3,as_array);
-            tlc_first.pwm(3,3,as_array2);
-            delay(20);
-            delay(on_time);
-            }
-
-          // check for keyboard every 16
-          if (!(h & 0xF)) { if (Serial.available() > 0) {Serial.println("Done"); return;} }
-
-          if (h==255) break; // h<=255 is ALWAYS true
-          }
-        // Serial.println("done h");
-        if (s==s_max) break; // h<=255 is ALWAYS true
-        }
-      if (v==255) break;
-      }
+byte choose_patch(byte current) {
+  for (byte i=0; i < Patch_Count; i++) {
+    print(i==current ? "=" : " ");print(" ");Serial.println(i);
     }
-  }
-
-void pwm_full_range() {
-  // while 0..7 goes up, 8..15 goes down & vice versa
-  const byte led_count = 6;
-  const byte size1 = 8;
-  byte direction;
-  byte values[16];
-  unsigned long timer = millis();
-
-  // init
-  for(byte i=0; i < size1; i++) { values[i] = 0; }
-  for(byte i=size1; i<=15; i++) { values[i] = 255; }
-
-  // vary
-  while (1) {
-    // Serial.print("Set 0..15 ");for(byte i=0; i<16; i++) {Serial.print(values[i]);Serial.print(" ");}
-    tlc_first.pwm(0,led_count, values);
-    delay(10);
-    // Serial.print(" next ");Serial.print(direction);Serial.println();
-
-    if (values[0] >= 255) { direction = -1; }
-    else if (values[0] <= 0) { 
-      Serial.print("Elapsed ");Serial.print(millis()-timer);Serial.println();
-      timer = millis();
-      direction = +1; 
-      }
-    if (! (values[0] & 0xf)) { if (Serial.available() > 0) break; }
-    for(byte i=0; i < size1; i++) { values[i] += direction; }
-    for(byte i=size1; i<=15; i++) { values[i] -= direction; }
-    
+  print(F("Choose: ")); 
+  while(Serial.available() <= 0) {}
+  char choice = Serial.read();
+  Serial.println(choice);
+  if (choice >= '0' && choice <= '0'+Patch_Count-1) {
+    print(F("Set to "));Serial.println(choice);
+    return choice-'0';
     }
+  Serial.println(F("What?"));Serial.println();
+  return current;
   }
-
-const int Slider_Pins[] = { 0,1,2,3,4,5,6,7,  8.9,10,11,12,13,14,15 };
-const byte Zones = 1;
-const byte Sample_Time = 100; // microsec
-// 50msec
-const int Sample_Interval = Sample_Time * Slider_ct > 50000 ? (Sample_Time * Slider_ct)/1000 : 50;
-const byte Lunits_Per_Zone = 2;
-const byte LEDs_Per_Lunit = 3;
-const byte Max_Lunit = 5;
-const byte Zone_Assignment[Slider_ct][Lunits_Per_Zone] = { // in Lunits (rgb #)
-  // 0
-  { 0, 1 },
-  // 1
-  { 2,3 },
-  // 2
-  { 4, -1 } // -1 is filler
-  };
-const byte RGB_Assignment[Slider_ct] = { //0,1,2 as rgb
-  0,1,2
-  };
-
-TLC59116 *dandelions[] = {&tlc_first};
-byte Targets[Slider_ct];
-const byte Total_LEDs = LEDs_Per_Lunit * Max_Lunit;
-byte leds[Total_LEDs];
-
-
-void grab_next_slider() {
-  // timer routine
-  for(byte i=0; i<Slider_ct ; i++) {
-    Targets[i] = analogRead(Slider_Pins[i]) >> 2;
-    }
-  }
-
-
-void set_pwm(byte* leds) {
-  // any assumption of led->actual? 16 per, continous
-  byte number_of_dandelions = (Total_LEDs + 15) / 16;
-  Serial.print("d_ct ");Serial.println(number_of_dandelions);
-  for(byte dandelion_i = 0; dandelion_i < number_of_dandelions; dandelion_i++) {
-    byte start_led_i = dandelion_i * 16;
-    TLC59116* dandelion = dandelions[dandelion_i];
-    dandelion->pwm(&leds[start_led_i]);
-    Serial.print("D ");Serial.print(dandelion_i);
-    for(byte led_num = start_led_i; led_num < start_led_i + 16; led_num++) {
-      Serial.print(leds[led_num]);Serial.print(" ");
-      }
-    Serial.println();
-    }
-  }
-  
-//algorithms
-inline void accumulatepwm_rgb_groups(byte slider_i) {
-  // maps a slider_i to the proper rgb element of zoned lunits
-  const byte* lunit_list = Zone_Assignment[ slider_i / 3 ]; // "zone x"
-  byte rgb_i = RGB_Assignment[slider_i % 3]; // "red of..."
-  
-  Serial.print("Accum for rgbi ");Serial.print(rgb_i);
-  for (byte i=0; i<Lunits_Per_Zone; i++) {
-    byte lunit = lunit_list[i];
-    if (lunit >= 0) {
-      Serial.print("lunit ");Serial.print(lunit);Serial.print(" for ");Serial.print(i);
-      byte t_led_num = lunit * LEDs_Per_Lunit + rgb_i; // "red of zone x"
-      Serial.print(" led num ");Serial.print(t_led_num); Serial.print("=");Serial.print(Targets[slider_i]);
-      leds[t_led_num] = Targets[slider_i];
-      }
-    }
-  Serial.println();
-  }
-*/
